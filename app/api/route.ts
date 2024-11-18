08b785d4c3bc93b52b26fb533783cb6b65679a28
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-11-17 20:02:42 -0500

    Add email template component and update dependencies

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import { EmailTemplate } from "@/components/email-template";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   try {
@@ -7,8 +11,20 @@ export async function POST(req: NextRequest) {
     // Log the data or use it however you need
     console.log("Form data received:", body);
 
+    // Send an email using the Resend API
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["hana.habibpor@gmail.com"],
+      subject: "SOP writer",
+      react: EmailTemplate({ name: body.name }),
+    });
+
+    if (error) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+
     return NextResponse.json(
-      { message: "Form data received successfully!", data: body },
+      { message: "Form data received successfully!", data: data },
       { status: 200 }
     );
   } catch (error) {

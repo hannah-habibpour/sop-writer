f2edc9e943bd2fdc62d8e4bb9e8093b91be68800
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-12-01 00:42:01 -0500

    Add react icon for required form inputs

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -1,3 +1,4 @@
+import { LuAsterisk } from "react-icons/lu";
 import { QuestionType } from "@/contracts/types";
 
 interface FormInputProps {
@@ -8,9 +9,14 @@ export default function FormInput({ question }: FormInputProps) {
   return (
     <div>
       <div className="flex flex-col my-[8px]">
-        <label htmlFor={question.inputId}>{question.question}</label>
+        <div className="flex items-center">
+          <label htmlFor={question.inputId}>{question.question}</label>
+          {question.isRequired && (
+            <LuAsterisk className="text-[12px] text-red-600" />
+          )}
+        </div>
         <input
-          className="border border-gray-300 rounded-md placeholder:text-sm"
+          className="border border-gray-600 rounded-md py-[2px] placeholder:text-sm placeholder:pl-[10px]"
           type={question.type}
           id={question.inputId}
           name={question.inputId}
